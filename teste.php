b7c0d8a879c2b3a1df42d130435928c16106aa8a
João Victor Rocha Batista <joao.batista@localhost.localdomain>
2018-02-19 14:26:37 -0300

    Adaptei o formulario, criando o back-end em php, que irá receber os dados do formulario

--- teste.php
+++ teste.php
@@ -1,8 +1,31 @@
 <?php
-
-if ($_POST['enviar']) {
-	$nome = $_POST['nome'];
+ini_set("display_errors",1);
+if (isset($_POST['cadastrar'])) {
+	echo $_POST['nome'];
+	echo "<br>";
+	echo $_POST['email'];
+	echo "<br>";
+	echo $_POST['nascimento'];
+	echo "<br>";
+	echo $_POST['sexo'];
+	echo "<br>";
+	echo $_POST['estado'];
+	echo "<br>";
+	echo $_POST['senha'];
+	echo "<br>";
+	echo $_POST['termo'];
 } else {
-	$nome = 'preencha';
+	echo 'Preencha com seu nome';
+	echo "<br>";
+	echo 'Preencha com seu email';
+	echo "<br>";
+	echo 'Preencha com sua data de nascimento';
+	echo "<br>";
+	echo 'Selecione seu sexo';
+	echo "<br>";
+	echo 'Selecione seu estado';
+	echo "<br>";
+	echo 'Preencha com sua senha';
+	echo "<br>";
+	echo 'Leia o termo e selecione o checkbox';
 }
-include('seila.php');
